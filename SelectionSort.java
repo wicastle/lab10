deffa018306989d36dedd210aaf9c5d5444ca6a9
William <wdcastle201@gmail.com>
2020-12-03 16:08:04 -0500

    mixed/dupli

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,25 +1,25 @@
 
-public class SelectionSort 
+public class SelectionSort
 {
 	private int temp;
-	
+
 	/** Creates a new instance of SelectionSort */
-	
-	public SelectionSort() 
+
+	public SelectionSort()
 	{
-		
-	}    
-	/* A simple SelectionSort algorithm     
-	 * pre-condition:     
-	 * post-condition:   
-	 * inputs:   
-	 * outputs:    
-	 * special conditions:   
-	 */   
-	
-	public int[] basicSelectionSort(int[] x) 
+
+	}
+	/* A simple SelectionSort algorithm
+	 * pre-condition:
+	 * post-condition:
+	 * inputs:
+	 * outputs:
+	 * special conditions:
+	 */
+
+	public int[] basicSelectionSort(int[] x)
 	{
-		for (int i = 0; i < x.length; i++) 
+		for (int i = 0; i < x.length; i++)
 		{
 			for (int j= i+1; j < x.length; j++)
 			{
@@ -27,7 +27,7 @@ public class SelectionSort
 				{
 					temp = x[i];
 					x[i] = x[j];
-					temp = x[j];
+					x[j] = temp;
 				}
 			} // end of inner for loop
 		}// end of outer for loop
